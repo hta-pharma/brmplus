da95988c8398d387e5eef24d26409da79318ad22
Jingxin-y <yanjingxin409@163.com>
2025-06-09 11:40:04 -0400

    Update MLE_Point_with_Hessian_for_RR.R

--- compare/MLE_Point_with_Hessian_for_RR.R
+++ compare/MLE_Point_with_Hessian_for_RR.R
@@ -127,6 +127,7 @@ compute.components = function(x, alpha.ml, beta.ml, va, vb, weight) {
   return(list(fisher = fisher.info,fisher.invers = solve(fisher.info),k.stu = cbind(k.aaa, k.aab, k.abb, k.bbb),k.s.tu = cbind(k.a.aa, k.a.ab, k.a.bb, k.b.aa, k.b.ab, k.b.bb)))
 }
 
+#' @param components A list as returned by \code{\link{compute.components}}.
 ### calculate κ^{r,s} κ^{t,u} (κ_{s,t,u} + κ_{s,tu}) / 2 with real va and vb. Since it is all the possible combinations of va and vb,I use "for"
 
 compute.augmentation <- function(components,va,vb){
