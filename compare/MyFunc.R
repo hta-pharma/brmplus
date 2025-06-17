821039351e4dd96646acd97231fcc4bbc15630e4
Jingxin-y <yanjingxin409@163.com>
2025-06-17 19:07:54 -0400

    Update MyFunc.R

--- compare/MyFunc.R
+++ compare/MyFunc.R
@@ -33,7 +33,7 @@ WrapResults = function(point.est, cov, param, name, va, vb, converged) {
 
     # The following two lines use the original method (Wald) from the package, 
     # but they can be replaced with the profile method(CI_LRT.R) or the exact method(CI_exact.R). 
-    # We can add an additional input parameter, such as "method_CI", 
+    # We can add an additional argument, such as "method_CI", 
     # and based on its value, call the corresponding function from the "CI_..." file.
     conf.lower = point.est + stats::qnorm(0.025) * se.est
     conf.upper = point.est + stats::qnorm(0.975) * se.est    
