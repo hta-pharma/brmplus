86e025f78eee87c0a0aee96e6c351f0e403cd8cc
cklamann <12862284+cklamann@users.noreply.github.com>
2025-07-29 14:49:33 -0400

    run styler to format all code

--- tests/testthat/test-brm.R
+++ tests/testthat/test-brm.R
@@ -1,26 +1,28 @@
 # sanity checks for now
 
 set.seed(0)
-n = 100
-alpha.true = c(0,-1)
-beta.true = c(-0.5, 1)
-gamma.true = c(0.1,-0.5)
-params.true = list(alpha.true = alpha.true,
-                   beta.true = beta.true,
-                   gamma.true = gamma.true)
-v.1 = rep(1, n) # intercept term
-v.2 = runif(n,-2, 2)
-v = cbind(v.1, v.2)
-pscore.true = exp(v %*% gamma.true) / (1 + exp(v %*% gamma.true))
-p0p1.true = getProbRR(v %*% alpha.true, v %*% beta.true)
-x = rbinom(n, 1, pscore.true)
-pA.true = p0p1.true[, 1]
-pA.true[x == 1] = p0p1.true[x == 1, 2]
-y = rbinom(n, 1, pA.true)
+n <- 100
+alpha.true <- c(0, -1)
+beta.true <- c(-0.5, 1)
+gamma.true <- c(0.1, -0.5)
+params.true <- list(
+  alpha.true = alpha.true,
+  beta.true = beta.true,
+  gamma.true = gamma.true
+)
+v.1 <- rep(1, n) # intercept term
+v.2 <- runif(n, -2, 2)
+v <- cbind(v.1, v.2)
+pscore.true <- exp(v %*% gamma.true) / (1 + exp(v %*% gamma.true))
+p0p1.true <- getProbRR(v %*% alpha.true, v %*% beta.true)
+x <- rbinom(n, 1, pscore.true)
+pA.true <- p0p1.true[, 1]
+pA.true[x == 1] <- p0p1.true[x == 1, 2]
+y <- rbinom(n, 1, pA.true)
 
 test_that("example runs correctly with MLE", {
-  r = brm(y, x, v, v, 'RR', 'MLE', v, TRUE)
-  expected_result = c(
+  r <- brm(y, x, v, v, "RR", "MLE", v, TRUE)
+  expected_result <- c(
     "alpha 1" = -0.78035527,
     "alpha 2" = -1.63433532,
     "beta 1" = -1.06854529,
@@ -30,8 +32,8 @@ test_that("example runs correctly with MLE", {
 })
 
 test_that("example runs correctly with DR", {
-  r = brm(y, x, v, v, 'RR', 'DR', v, TRUE)
-  expected_result = c(
+  r <- brm(y, x, v, v, "RR", "DR", v, TRUE)
+  expected_result <- c(
     "alpha 1" = -0.73984903,
     "alpha 2" = -1.33843580,
     "beta 1" = -1.06854529,
