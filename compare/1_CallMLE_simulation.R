7113c37f884773a1aafbd1290470a1f9e69d2fa4
cklamann <12862284+cklamann@users.noreply.github.com>
2025-11-04 14:24:10 -0500

    correct jeffrey methods and add CI argument to callMLE_sim

--- compare/1_CallMLE_simulation.R
+++ compare/1_CallMLE_simulation.R
@@ -1,5 +1,5 @@
 MLEst = function(param, y, x, va, vb, weight, max.step, thres, alpha.start,
-    beta.start, pa, pb, method = "brm") {
+    beta.start, pa, pb, method = "brm", CI = "wald") {
 
     ## starting values for parameter optimization
     if (is.null(alpha.start))
@@ -34,10 +34,10 @@ MLEst = function(param, y, x, va, vb, weight, max.step, thres, alpha.start,
                   weight, max.step, thres, pa, pb)
           }
       } else if(method == "jeffrey-p"){
-            mle = max.likelihood.jeffrey(param, y, x, va, vb, alpha.start, beta.start,
+            mle = max.likelihood.jeffrey.direct(param, y, x, va, vb, alpha.start, beta.start,
                   weight, max.step, thres, pa, pb)
       } else if(method == "jeffrey-est"){
-            mle = max.likelihood.jeffrey.direct(param, y, x, va, vb, alpha.start, beta.start,
+            mle = max.likelihood.jeffrey(param, y, x, va, vb, alpha.start, beta.start,
                   weight, max.step, thres, pa, pb)
       } else {
           stop(paste0(method, " is not a recognized method!"))
@@ -56,12 +56,32 @@ MLEst = function(param, y, x, va, vb, weight, max.step, thres, alpha.start,
           cov = var.mle.rd(x, alpha.ml, beta.ml, va, vb, weight)
       }
       sd.est = sqrt(diag(cov))
+    }
 
+    conf.lower = point.est + stats::qnorm(0.025) * sd.est
+    conf.upper = point.est + stats::qnorm(0.975) * sd.est
+    p.temp = stats::pnorm(point.est/sd.est, 0, 1)
+    p.value = 2 * pmin(p.temp, 1 - p.temp)
+
+    if(CI == "wald"){
+      ci.est = list(low = conf.lower,
+                    up = conf.upper,
+                    p = p.value)
+    }else if(CI == "exact"){
+      ci.est.alpha = exact(param,y, x, va, vb, weight, max.step, thres, thres.dicho=1e-3, point.est, sd.est, pa, pb)
+      ci.est = list(low = c(ci.est.alpha$low,conf.lower[(pa+1):(pa+pb)]),
+                    up = c(ci.est.alpha$up,conf.upper[(pa+1):(pa+pb)]),
+                    p = c(ci.est.alpha$p,p.value[(pa+1):(pa+pb)]))
+    }else if(CI == "LRT"){
+      ci.est.alpha = profile(param,y, x, va, vb, weight, max.step, thres, point.est, sd.est, pa, pb)
+      ci.est = list(low = c(ci.est.alpha$low,conf.lower[(pa+1):(pa+pb)]),
+                    up = c(ci.est.alpha$up,conf.upper[(pa+1):(pa+pb)]),
+                    p = c(ci.est.alpha$p,p.value[(pa+1):(pa+pb)]))
+    }else {
+      stop(paste0(CI, " is not a recognized method!"))
     }
 
     name = paste(c(rep("alpha", pa), rep("beta", pb)), c(1:pa, 1:pb))
-    sol = WrapResults(point.est, cov, param, name, va, vb, converged)
+    sol = WrapResults(point.est, cov, param, name, va, vb, converged, ci.est)
     return(sol)
-
 }
-
