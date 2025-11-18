fa771537fd707e787e12e2a1e9bc5932885d82f9
cklamann <12862284+cklamann@users.noreply.github.com>
2025-11-18 10:34:21 -0500

    style tests

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -1,17 +1,17 @@
 set.seed(0)
 
 test_that("expit function works", {
-  p <- runif(0,1)
+  p <- runif(0, 1)
   l <- log(p) - log(1 - p)
   i <- expit(l)
-  expect_equal(i, 1/l)
+  expect_equal(i, 1 / l)
 })
 
 test_that("same function works", {
-  expect_true(same(5,5))
-  expect_false(same(5,5 + .Machine$double.eps^0.5))
-  expect_true(same(-5,5, 10.1))
-  expect_false(same(1,2,.5))
+  expect_true(same(5, 5))
+  expect_false(same(5, 5 + .Machine$double.eps^0.5))
+  expect_true(same(-5, 5, 10.1))
+  expect_false(same(1, 2, .5))
 })
 
 test_that("get_prob_aux function works", {
